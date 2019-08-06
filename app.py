01d19d04fe5969b30bee04a984a7d3d76f3383c4
Ubuntu <ubuntu@ip-172-31-80-58.ec2.internal>
2019-08-06 14:24:22 +0000

    initial commit

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 import os
-from flask import Flask
+
+from flask import Flask, render_template, redirect, request, url_for
+
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+# Mongo Database for The Global Irish Cafe
+app.config["MONGO_DBNAME"] = "global_irish_cafe"
+app.config["MONGO_URI"] = 'mongodb+srv://KittyOwner:Stephbar2@kittysfirstcluster-f9urv.mongodb.net/global_irish_cafe?retryWrites=true&w=majority'
+
+mongo = PyMongo(app)
+
 @app.route('/')
-def hello():
-    return "Hello World . . . again"
+@app.route('/get_recipes')
+def get_recipes():
+    
+    # Redirect to recipes template, return all recipes
+    return render_template("recipes.html", recipes=mongo.db.recipes.find())
 
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
