e6c6e76265e22a630a2aa00254fd62d835e5eb33
Ubuntu <ubuntu@ip-172-31-88-100.ec2.internal>
2019-09-30 10:05:07 +0000

    intro case changed

--- app.py
+++ app.py
@@ -443,7 +443,7 @@ def insert_recipe():
     recipe = {"origin" : request.form['origin'].lower(), 
               "category" : request.form['category'].lower(),
               "title" : request.form['title'].lower(),
-              "intro" : request.form['intro'].lower(),
+              "intro" : request.form['intro'],
               "owner" : request.form['owner'].lower(),
               "prep_time" : request.form['prep_time'],
               "serves" : request.form['serves'],
